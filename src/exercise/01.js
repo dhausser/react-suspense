05c178fc7a746de31f89497a52aa7f16ee0a0794
Davy Hausser <davy.hausser@gmail.com>
2021-02-05 23:49:31 +0100

    Simple Data-fetching

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,28 +4,17 @@
 import * as React from 'react'
 import {PokemonDataView, fetchPokemon} from '../pokemon'
 
-// fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
 let pokemon
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
 const pokemonPromise = fetchPokemon('pikachu')
-pokemonPromise.then(resolvedValue => (pokemon = resolvedValue))
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+pokemonPromise.then(
+  resolvedValue => (pokemon = resolvedValue),
+  error => console.log(new Error(error)),
+)
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
-
   if (!pokemon) throw pokemonPromise
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
